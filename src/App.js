f9e5d8d8e908217a10d00162310f30df48e13d59
SNiemiec <slawekniemiec@gmail.com>
2019-09-07 00:15:10 +0200

    Add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 
 class App extends Component {
   state = {
-    success: false
+    success: true,
+    guessedWords: [{guessedWord: 'train', letterMatchCount: 3}]
   };
 
   render() {
     return (
-      <div>
+      <div className="container">
+        <h1>Jotto</h1>
         <Congrats success={this.state.success} />
+        <GuessedWords guessedWords={this.state.guessedWords} />
       </div>
     );
   }
--- src/Congrats.js
+++ src/Congrats.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const Congrats = (props) => {
  
     return (
-      <div data-test="component-congrats">
+      <div data-test="component-congrats" className="alert alert-success">
         <span data-test="message-congrats">
           {props.success? 'Congratulations! You guessed the word!' : null }
         </span>
